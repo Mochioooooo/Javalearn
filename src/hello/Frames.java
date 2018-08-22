7deef7bddcd0bef82d6008e58f90b9787e513a29
mochiooooo <ricecakeluna@yahoo.com>
2018-08-22 23:30:49 +0900

    Initial import Java Project.
    Signed-off-by: mochiooooo <ricecakeluna@yahoo.com>

---
+++ src/hello/Frames.java
@@ -0,0 +1,14 @@
+package hello;
+import javax.swing.*;
+
+public class Frames {  
+	public static void main(String[] args) {
+		JFrame frame = new JFrame( " Hello Java!" );
+		JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
+		frame.add(label);
+		frame.setSize(300, 300);
+		frame.setVisible( true);
+				
+	}
+
+}
